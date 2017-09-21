8670c96f891e4092d2088a3f98289d6c2a825fdb
Jacob Harrison Lyon <jacobhlyon@gmail.com>
2017-09-21 10:55:57 -0400

    user can log in with react

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 	namespace :api do
 		namespace :v1 do
 			resources :users
-			post '/login' to: 'auth#create'
+			post '/login', to: 'auth#create'
 			get '/me', to: 'users#me'
 		end
 	end
