7bc9b72f99582682681f18684b0f2f207bbce529
Jacob Harrison Lyon <jacobhlyon@gmail.com>
2017-09-25 17:33:52 -0400

    added wine routes

--- app/controllers/api/v1/wines_controller.rb
+++ app/controllers/api/v1/wines_controller.rb
@@ -1,1 +1,20 @@
 class Api::V1::WinesController < ApplicationController
+
+	def index
+		wines = Wine.all
+		render json: wines
+	end
+
+	def create
+	    wine = Wine.create(wine_params)
+	    render json: wine
+	  end
+
+
+  private
+
+  def wine_params
+    params.require(:data).permit(:name, :vintage, :varietal, :blend, :type, :image, :winery, :snooth_rank, :region)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 	namespace :api do
 		namespace :v1 do
 			resources :users
+			resources :wines
 			post '/login', to: 'auth#create'
 			get '/me', to: 'users#me'
 		end
