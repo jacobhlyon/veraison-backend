0f7216bcda05c5a186d243e7c527044e34dd2c9f
Jacob Harrison Lyon <jacobhlyon@gmail.com>
2017-10-18 10:47:51 -0400

    figaro updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::API
 
 	def issue_token(payload)
-		JWT.encode(payload, Figaro.secret, "HS256")
+		JWT.encode(payload, ENV["secret"], "HS256")
 	end
 
 	def decode_token(token)
 		begin
-			JWT.decode(token, Figaro.secret, true, { :algorithm => 'HS256' })
+			JWT.decode(token, ENV["secret"], true, { :algorithm => 'HS256' })
 		rescue JWT::DecodeError
 			[]
 		end
